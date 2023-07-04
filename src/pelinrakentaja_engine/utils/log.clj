9cc94b9e85750f76973cdeefa0f777108609dfbc
Jukka Länsineva <jukka.lansineva@solita.fi>
2023-07-04 22:09:08 +0300

    a simple logger util

--- src/pelinrakentaja_engine/utils/log.clj
+++ src/pelinrakentaja_engine/utils/log.clj
@@ -2,11 +2,14 @@
 
 (def enabled-logs #{:all
                     :input-adapter/key-down
-                    :input-adapter/key-up})
+                    :input-adapter/key-up
+                    :event-handlers/key-down
+                    :event-handlers/key-up
+                    :engine/lifecycle})
 (def enabled-log-levels #{:all :important :debug :info})
 
 (defn log
-  [logger log-level & params]
+  [log-level logger & params]
   (when (and
           (or (logger enabled-logs)
               (:all enabled-logs))
