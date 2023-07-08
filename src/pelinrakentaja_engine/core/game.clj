dee8c588718939c0cc674aa03eccc1fd3abe933b
Jukka Länsineva <jukka.lansineva@solita.fi>
2023-07-08 16:53:12 +0300

    removed core-events from the game window initialization
    for clarity and cleanup

--- src/pelinrakentaja_engine/core/game.clj
+++ src/pelinrakentaja_engine/core/game.clj
@@ -2,8 +2,7 @@
   (:require [pelinrakentaja-engine.core.state :as state]
             [pelinrakentaja-engine.core.input :as input]
             [pelinrakentaja-engine.core.events :as events]
-            [pelinrakentaja-engine.core.core-events :as core-events]
-            [pelinrakentaja-engine.core.core-listeners :as core-listeners]
+
             [pelinrakentaja-engine.utils.log :as log])
   (:import [com.badlogic.gdx Gdx ApplicationAdapter]
            [com.badlogic.gdx.graphics GL20 OrthographicCamera]
@@ -20,7 +19,7 @@
   (let [{:keys [camera batch]} @game-data
         font (BitmapFont.)
         text "Testing"]
-    (events/update-queue)
+    #_(log/log :debug :events (:engine @events/state))
     (.glClearColor (Gdx/gl) 0.2 0.2 0 0)
     (.glClear (Gdx/gl) GL20/GL_COLOR_BUFFER_BIT)
     (.update camera)
@@ -40,19 +39,12 @@
   (log/log :debug :engine/lifecycle "creating")
   (let [camera (OrthographicCamera.)
         sprite-batch (SpriteBatch.)]
-    (events/register-handler :input/key-down core-events/key-down)
-    (events/register-handler :input/key-up core-events/key-up)
-    (events/register-handler :engine/initialize core-events/engine-initialized)
-    (events/register-handler :engine/ready core-events/engine-ready)
-    (events/register-handler :engine/cleanup core-events/engine-cleanup)
-    (events/register-listener :engine/status [:engine :status] core-listeners/engine-status)
-    (events/register-listener :input/pressed-keys [:input :keys] core-listeners/pressed-keys)
     (.setInputProcessor (. Gdx -input) (input/create-input-adapter))
     (.setToOrtho camera false 800 480)
     (swap! game-data assoc
            :camera camera
            :batch sprite-batch)
-    (events/dispatch [:engine/initialize])))
+    (events/force [:engine/initialize])))
 
 (defn -pause
   [^ApplicationAdapter this]
