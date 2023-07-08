2f23ba4fa1121723acadb97cd4c47ff7f4332ae4
Jukka Länsineva <jukka.lansineva@solita.fi>
2023-07-08 16:51:53 +0300

    logger messages are sent into queue and then are processed
    at once per update. this helps to avoid corruption of log messages
    due to multithreading.

--- src/pelinrakentaja_engine/utils/log.clj
+++ src/pelinrakentaja_engine/utils/log.clj
@@ -8,11 +8,21 @@
                     :engine/lifecycle})
 (def enabled-log-levels #{:all :important :debug :info})
 
+(def log-queue (atom []))
+
+(defn print-logs
+  []
+  (when-not (empty? @log-queue)
+    (swap! log-queue
+           #(do
+              (mapv prn %)
+              []))))
+
 (defn log
   [log-level logger & params]
   (when (and
-          (or (logger enabled-logs)
-              (:all enabled-logs))
-          (or (log-level enabled-log-levels)
-              (:all enabled-log-levels)))
-    (apply prn logger params)))
+         (or (logger enabled-logs)
+             (:all enabled-logs))
+         (or (log-level enabled-log-levels)
+             (:all enabled-log-levels)))
+    (swap! log-queue conj (apply str logger params))))
