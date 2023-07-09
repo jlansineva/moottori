4f41f025c4182dcf91d328c7d3058dfc97efc05c
Jukka Länsineva <jukka.lansineva@solita.fi>
2023-07-09 21:37:30 +0300

    refactored and removed unused code

--- src/pelinrakentaja_engine/core/state.clj
+++ src/pelinrakentaja_engine/core/state.clj
@@ -1,9 +1,5 @@
 (ns pelinrakentaja-engine.core.state
-  (:import [com.badlogic.gdx Gdx]
-           [com.badlogic.gdx.graphics Texture]))
-
-(defonce textures-for-type
-  (atom {}))
+  (:require [pelinrakentaja-engine.core.graphics.textures :as textures]))
 
 (defonce renderable-entities
   (atom {}))
@@ -11,18 +7,13 @@
 (defonce render-queue
   (atom []))
 
-(defn load-texture
-  [type-id path]
-  (swap! textures-for-type
-    assoc type-id (Texture. (.internal (. Gdx -files) path))))
-
 (defn add-entity
   [entity]
   (let [to-add (-> entity
                  (update :x float)
                  (update :y float)
                  (assoc :id (keyword (gensym (name (:type entity))))))]
-    (when (get @textures-for-type (:type entity))
+    (when (get @textures/textures-for-type (:type entity))
       (swap! renderable-entities assoc (:id to-add) to-add)
       (swap! render-queue conj (:id to-add))
       to-add)))
@@ -42,7 +33,7 @@
 
          [:terrain :enemies :bullets :player :ui])
 
-(defn load-entity
+#_(defn load-entity
   [entity]
   (let [{:keys [type texture]} entity]
     (load-texture type texture)))
