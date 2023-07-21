b5527bed5140de357e2620d2964e8971a8614159
Jukka Länsineva <jukka.lansineva@solita.fi>
2023-07-21 18:25:41 +0300

    modified texture loading for direct access

--- src/pelinrakentaja_engine/core/graphics/textures.clj
+++ src/pelinrakentaja_engine/core/graphics/textures.clj
@@ -1,13 +1,17 @@
 (ns pelinrakentaja-engine.core.graphics.textures
-  (:require [pelinrakentaja-engine.core.events :as events])
+  (:require [pelinrakentaja-engine.core.events :as events]
+            [pelinrakentaja-engine.utils.log :as log])
   (:import [com.badlogic.gdx Gdx]
            [com.badlogic.gdx.graphics Texture Texture$TextureWrap]
-   #_        [com.badlogic.gdx.graphics.Texture TextureWrap]
            [com.badlogic.gdx.graphics.g2d TextureRegion]))
+
+(def resource-loaded? (events/listener :resources/resource-loaded?))
 
 (defn load-texture-from-resource
   [id path]
-  (let [texture (Texture. (.internal (. Gdx -files) path))
-        _ (.setWrap texture Texture$TextureWrap/Repeat Texture$TextureWrap/Repeat)
-        region (TextureRegion. texture)]
-    (events/dispatch [:resources/resource-loaded :texture id region])))
+  (log/log :debug :id id :path path)
+  (when-not (resource-loaded? :texture id)
+    (let [texture (Texture. (.internal (. Gdx -files) path))
+          _ (.setWrap texture Texture$TextureWrap/Repeat Texture$TextureWrap/Repeat)]
+      (log/log :debug :texture :h (.getHeight texture) :w (.getWidth texture))
+      (TextureRegion. texture))))
