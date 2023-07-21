be577aa362c50b3c25e91a52e5e09f5942c4b0cb
Jukka Länsineva <jukka.lansineva@solita.fi>
2023-07-21 18:29:27 +0300

    updated project.clj and configs

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [nrepl/nrepl "1.1.0-alpha1"]
                  [com.badlogicgames.gdx/gdx "1.11.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl3 "1.11.0"]
                  [com.badlogicgames.gdx/gdx-box2d "1.11.0"]
---
+++ src/pelinrakentaja_engine/config.clj
@@ -0,0 +1,5 @@
+(ns pelinrakentaja-engine.config)
+
+;; TODO placehodler, need to implement some sort of loading
+(def world-scale-x 16)
+(def world-scale-y 16)
