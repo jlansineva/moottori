a4bfaa1dfe52c1af695a8367c71fa3b7d0502376
Jukka Länsineva <jukka@coldhandscreations.com>
2026-06-14 00:06:16 +0300

    removed dead parts of the codebase

--- src/pelinrakentaja_engine/core/graphics/sprites.clj
+++
@@ -1,7 +0,0 @@
-(ns pelinrakentaja-engine.core.graphics.sprites
-#_  (:import [com.badlogic.gdx Gdx]
-           [com.badlogic.gdx.graphics.g2d Sprite]))
-
-(defn create-sprite
-  [texture]
-  #_(Sprite. texture))
